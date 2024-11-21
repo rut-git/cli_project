262a039b60af393c267d4795465dff4ddcff6c70
Ruth Stefansky <165785128+rut-git@users.noreply.github.com>
2024-11-21 20:49:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,3 @@
-﻿/*using System.CommandLine;
-var bundleOption = new Option<FileInfo>("--output", "File path and name");
-var bundleCommand = new Command("bundle", "bundle code files to a sinle file");
-bundleCommand.AddOption(bundleOption);
-bundleCommand.SetHandler((output) =>
-{
-    try
-    {
-        File.Create(output.FullName);
-        Console.WriteLine("File was created");
-
-    }
-    catch (Exception ex) { }
-}, bundleOption);
-var rootCommand = new RootCommand("Root command for file bundler CLI");
-rootCommand.AddCommand(bundleCommand);
-rootCommand.InvokeAsync(args);
-*/
 using System.CommandLine;
 using System.IO;
 using System.Linq;
