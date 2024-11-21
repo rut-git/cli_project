eabe236512efe28d992298c006128c1a17cc91df
rut-git <r3208616@gmail.com>
2024-11-21 17:02:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.CommandLine;
+var bundleOption = new Option<FileInfo>("--output", "File path and name");
+var bundleCommand = new Command("bundle", "bundle code files to a sinle file");
+bundleCommand.AddOption(bundleOption);
+bundleCommand.SetHandler((output) =>
+{
+    try
+    {
+        File.Create(output.FullName);
+        Console.WriteLine("File was created");
+
+    }
+    catch (Exception ex) { }
+}, bundleOption);
+var rootCommand = new RootCommand("Root command for file bundler CLI");
+rootCommand.AddCommand(bundleCommand);
+rootCommand.InvokeAsync(args);
